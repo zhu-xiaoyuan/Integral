c8ebe7faafe22184a332a2fae82c3442f43dd4f9
ly <sweetheartly@foxmail.com>
2016-10-30 12:52:22 +0800

    merge liyun

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,6 +3,37 @@ namespace Admin\Controller;
 class IndexController extends BaseController {
     public function index(){
         $this->display();
-//        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>[ 您现在访问的是Admin模块的Index控制器 ]</div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+    }
+    // 修改密码
+    public function changeInfo(){
+    	$this->display();
+    }
+    // 修改
+    public function changePass(){
+    	$office = M("office");
+    	$map['o_id'] = $_SESSION['o_id'];	//获取当前用户的id
+    	$old = $office->where($map)->field('o_psd')->find();	//找到当前用户的密码
+    	if ($old) {
+	    	$change_new = md5($_POST['c_NewPsd']);
+	    	$change_newAgain = md5($_POST['c_NewPsdAgain']);
+	    	if ($change_new == $change_newAgain) {	//重复输入密码一致
+    			$change_old = md5($_POST['c_Psd']);	//获取数据库中的密码
+	    		if ($change_old == $old['o_psd']) {	//原密码正确
+					$data['o_psd'] = md5($_POST['c_NewPsd']);
+					$re = $office->where($map)->save($data);	// 修改密码
+					if ($re) {
+						$this->ajaxReturn(5); // 修改成功
+					}else{
+						$this->ajaxReturn(6);	// 修改失败
+					}
+				}else{
+					$this->ajaxReturn(7);	// 原密码输入有误
+				}
+	    	}else{
+	    		$this->ajaxReturn(8);	// 两次密码不一致
+	    	}
+    	}else{
+    		$this->ajaxReturn(9);	// 数据错误
+    	}
     }
 }
