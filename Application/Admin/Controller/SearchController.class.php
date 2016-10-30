e1a8b5835ed885762e47644d16950590a7d729a7
ACreateDog <2494662681@qq.com>
2016-10-30 12:32:16 +0800

    微信段完成

--- Application/Admin/Controller/SearchController.class.php
+++ Application/Admin/Controller/SearchController.class.php
@@ -11,27 +11,147 @@ header('ManegeController.class.php');
 class SearchController extends  BaseController{
 
     function _empty($id){
-
         $is_num = is_numeric($id);
         if($is_num){
-            echo $id;
-            $info = $this->get_score_by_id($id);
-            dump($info);
+//            echo $id;
+//            $info = $this->get_class_ranking('20141514111','计科141');
+//            dump($info);
         }else{
             echo '非数字';
         }
     }
-    function get_score_by_id($id){
-    	$manage = new ManageController();
+    function index(){
+        $this->display();
+    }
+    function search(){
+        $s_id = null;
+        if(!empty($_POST)){
+            $s_id = $_POST['s_id'];
+            $info = $this->get_score_by_id($s_id);
 
-    	$model = M('scoredetail');
-    	if (!empty($id)) {
-    		if ($manage->student_is_exist($id)) {
-    			$info = $model->where('s_id='.'"'.$id.'"')->select();
-    			
-    			return $info;
-    		} 
-    	}
+        }
+
+        $this->assign('list',$info);
+        $this->assign('s_id',$s_id);
+        $this->display();
+    }
+    function personScoreDetail(){
+        $info = null;
+        $id = null;
+
+        if(!empty($_POST)){
+            $id = $_POST['s_id'];
+        }else{
+            $id = $_GET['s_id'];
+        }
+        if(!empty($id)){
+            $model = M('scoredetail');
+            $manage = new ManageController();
+            if ($manage->student_is_exist($id)) {
+                $info = $model->where('s_id=' . '"' . $id . '"')->select();
+            }
+        }
+
+        $title = '个人积分详情';
+        $this->viewTheDetail($info,$title,1);
 
     }
+    function classScoreDetail(){
+
+        $info = null;
+
+        if(!empty($_POST)){
+            $class_name = $_POST['c_name'];
+            if (!empty($class_name)) {
+                $info = $this->get_class_detail($class_name);
+            }
+        }
+        $title = '班级积分详情';
+        $this->viewTheDetail($info,$title,0);
+    }
+    function viewTheDetail($info,$title,$isperson){
+        $this->assign('title',$title);
+        $this->assign('isPerson',$isperson);
+        $this->assign('list',$info);
+
+        $this->display('viewTheDetail');
+    }
+    function get_score_by_id($id){
+
+    	$model = M('scoredetail');
+        $manage = new ManageController();
+    	if (!empty($id)) {
+    		if ($manage->student_is_exist($id)) {
+                $info = $model->where('s_id='.'"'.$id.'"')->select();
+                $class_name = $info[0]['c_name'];
+                $data_class = $this->get_class_ranking($id,$class_name);
+                $major_name = $this->get_major($class_name);
+                $data_major = $this->get_major_ranking($id,$major_name);
+
+                $data['id'] = $data_class['sid'];
+                $data['class_rank'] = $data_class['rank'];
+                $data['major_rank'] = $data_major['rank'];
+                $data['all_score'] = $data_major['all_score'];
+                $data['class_name'] = $class_name;
+    			return $data;
+    		}
+    	}
+    }
+
+    function  get_major($class_name){
+        if(!empty($class_name)){
+            $major_name = preg_replace('/[0-9]/','',$class_name);
+            return $major_name;
+        }
+    }
+    function  get_major_detail($major){
+        $model = M('scoredetail');
+        $info = $model->field('s_id,sum(sc_number)')->where('c_name like'.'"'.$major.'%"')->order('sum(sc_number) desc')->group('s_id')->select();
+        return $info;
+    }
+    function  get_class_detail($class_name){
+        $model = M('scoredetail');
+        $info = $model->field('s_id,s_name,sum(sc_number)')->where('c_name like'.'"'.$class_name.'"')->order('sum(sc_number) desc')->group('s_id')->select();
+        foreach($info as $k => $v){
+
+            $info[$k]['all_score'] = sprintf("%.2f",floatval( $v['sum(sc_number)']));
+        }
+        return $info;
+    }
+    /*
+     * 获取专业排名
+     * */
+    function get_major_ranking($id,$major){
+        $detal_rank = array();
+        $info = $this->get_major_detail($major);
+        if(!empty($info)){
+            foreach($info as $key => $value){
+                if($value['s_id'] == $id){
+                    $detal_rank['rank'] = $key;
+                    $detal_rank['sid']  = $id;
+                    $detal_rank['all_score'] = sprintf("%.2f",floatval( $value['sum(sc_number)']));
+                    return $detal_rank;
+                }
+            }
+        }
+    }
+    /*
+     * 获取班级排名
+     * */
+    function get_class_ranking($id,$class_name){
+        $detal_rank = array();
+        $info = $this->get_class_detail($class_name);
+        if(!empty($info)){
+            foreach($info as $key => $value){
+                if($value['s_id'] == $id){
+                    $detal_rank['rank'] = $key;
+                    $detal_rank['sid']  = $id;
+                    $detal_rank['all_score'] = sprintf("%.2f",floatval( $value['sum(sc_number)']));
+                    return $detal_rank;
+                }
+            }
+        }
+    }
+
+
 }
--- Application/Admin/Controller/WechatController.class.php
+++ Application/Admin/Controller/WechatController.class.php
@@ -1,19 +1,19 @@
 <?php
+
+namespace Admin\Controller;
+use Think\Controller;
 /**
   * wechat php test
   */
 
-namespace Admin\Controller;
-use Think\Controller;
 //define your token
 define("TOKEN", "weixin");
-$wechatObj = new wechatCallbackapiTest();
-$wechatObj->responseMsg();
 
-class wechatCallbackapiTest
+class WechatController
 {
 	public function valid()
     {
+
         $echoStr = $_GET["echostr"];
 
         //valid signature , option
@@ -33,11 +33,13 @@ class wechatCallbackapiTest
                 /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
                    the best way is to check the validity of xml by yourself */
                 libxml_disable_entity_loader(true);
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+              	$postObj      = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
                 $fromUsername = $postObj->FromUserName;
-                $toUsername   =  $postObj->ToUserName;//微信公众平台
+                $toUsername   = $postObj->ToUserName;//微信公众平台
                 $keyword      = trim($postObj->Content);//用户的发送的内容
                 $time         = time();
+                $msgType      = $postObj->MsgType; //判断用户发送的消息是文本消息
+
                 $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
 							<FromUserName><![CDATA[%s]]></FromUserName>
@@ -46,34 +48,21 @@ class wechatCallbackapiTest
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
 							</xml>";             
-				if(!empty( $keyword ))
-                {
-                    $search = new SearchController();
-                    $info = $search->get_score_by_id($keyword);
-              		$msgType = "text";
-//                	$contentStr = "Welcome to wechat world!";
-                    $relt = $this->result($info);
-                    $contentStr = '你的总积分是'.$relt.'分';
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
+				if ($msgType == "text") {
+                    if(!empty( $keyword ))
+                    {
+                        $msgType    = "text";
+                        $contentStr = "Welcome to wechat world!";
+                        //拼接XML字符串
+                        $resultStr  = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                        echo $resultStr;
+                    }else{
+                        echo "Input something...";
+                    }  
                 }
-
         }else {
         	echo "";
         	exit;
-        }
-    }
-    public function result($info)
-    {   $data = null;
-		$all_score = .0;
-        if (!empty($info)) {
-            foreach ($info as $value){
-                // $value['s_name'].$value['c_name'].$value['']
-                $all_score+= floatval($value['sc_number']);
-            }
-            return $all_score;
         }
     }
 		
@@ -86,14 +75,14 @@ class wechatCallbackapiTest
         
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
+        $nonce     = $_GET["nonce"];
         		
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
+		$token     = TOKEN;
+		$tmpArr    = array($token, $timestamp, $nonce);
         // use SORT_STRING rule
 		sort($tmpArr, SORT_STRING);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
+		$tmpStr    = implode( $tmpArr );
+		$tmpStr    = sha1( $tmpStr );
 		
 		if( $tmpStr == $signature ){
 			return true;
@@ -103,4 +92,6 @@ class wechatCallbackapiTest
 	}
 }
 
+$wechatObj = new WechatController();
+$wechatObj->responseMsg();
 ?>
